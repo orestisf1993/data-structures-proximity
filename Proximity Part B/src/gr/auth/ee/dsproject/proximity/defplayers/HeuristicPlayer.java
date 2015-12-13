8c50e1490dffbedbb24ce8f8f2e6763cf25611d6
orestisf <orestisf1993@gmail.com>
2015-12-13 04:46:31 +0200

    Add HashMap for opponent's available moves
    Updates on getNextMove() with updateOpponentsPool() by removing last played move taken from
    board.getOpponentsLastMove().

--- Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/HeuristicPlayer.java
+++ Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/HeuristicPlayer.java
@@ -1,5 +1,7 @@
 package gr.auth.ee.dsproject.proximity.defplayers;
 
+import java.util.HashMap;
+
 import gr.auth.ee.dsproject.proximity.board.Board;
 import gr.auth.ee.dsproject.proximity.board.ProximityUtilities;
 import gr.auth.ee.dsproject.proximity.board.Tile;
@@ -11,15 +13,15 @@ public class HeuristicPlayer implements AbstractPlayer {
     int opponentId;
     String name;
     int numOfTiles;
+    private HashMap<Integer, Integer> opponentsPool = new HashMap<Integer, Integer>();
 
     public HeuristicPlayer(final Integer pid) {
         id = pid;
         opponentId = (pid == 1) ? 2 : 1;
     }
 
-    double getEvaluation(final Board board, final int randomNumber, final Tile tile) {
+    public double getEvaluation(final Board board, final int randomNumber, final Tile tile) {
         final Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
-
         int scoreFromAlies = 0;
         int scoreFromEnemies = 0;
 
@@ -48,6 +50,7 @@ public class HeuristicPlayer implements AbstractPlayer {
     public int[] getNextMove(final Board board, final int randomNumber) {
         double max = -1;
         final int[] result = new int[3];
+        updateOpponentsPool(board);
         for (int i = 0; i < ProximityUtilities.NUMBER_OF_ROWS; i++) {
             for (int j = 0; j < ProximityUtilities.NUMBER_OF_COLUMNS; j++) {
                 final Tile tile = board.getTile(j, i);
@@ -77,13 +80,11 @@ public class HeuristicPlayer implements AbstractPlayer {
     }
 
     public void setId(final int id) {
-        // TODO Auto-generated method stub
         this.id = id;
 
     }
 
     public void setName(final String name) {
-        // TODO Auto-generated method stub
         this.name = name;
 
     }
@@ -94,6 +95,23 @@ public class HeuristicPlayer implements AbstractPlayer {
 
     public void setScore(final int score) {
         this.score = score;
+    }
+
+    private void updateOpponentsPool(Board board) {
+        int[] lastMove = board.getOpponentsLastMove();
+        if (lastMove[0] == -1 && lastMove[1] == -1) {
+            // init opponentsPool
+            for (Integer key = 1; key <= 20; key++) {
+                opponentsPool.put(key, 3);
+            }
+        } else {
+            // decrease by 1.
+            Tile lastMoveTile = board.getTile(lastMove[0], lastMove[1]);
+            assert (opponentId == lastMoveTile.getPlayerId());
+            Integer key = lastMoveTile.getScore();
+            Integer value = opponentsPool.get(key) - 1;
+            opponentsPool.put(key, value);
+        }
     }
 
 }
