d0d7cdbdeee2a76d2e082cb630bf9139d2e2d843
orestisf <orestisf1993@gmail.com>
2015-12-14 02:42:53 +0200

    Fix calculateRisk() to use the map key for comparison

--- Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/HeuristicPlayer.java
+++ Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/HeuristicPlayer.java
@@ -33,7 +33,7 @@ public class HeuristicPlayer implements AbstractPlayer {
         }
     }
 
-    double calculateRisk(Tile tile, Board board) {
+    private double calculateRisk(Tile tile, Board board) {
         Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
         HashMap<Integer, Integer> map;
         if (opponentId == tile.getPlayerId()) {
@@ -45,26 +45,28 @@ public class HeuristicPlayer implements AbstractPlayer {
         double emptyNeighbors = 0;
         double scoreTile = tile.getScore();
         for (Tile neighbor : neighbors) {
-            if (neighbor != null && neighbor.getPlayerId() == 0)
+            if (neighbor != null && neighbor.getPlayerId() == 0) {
                 emptyNeighbors++;
+            }
         }
 
         double totalValues = 0;
         double biggerValues = 0;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            totalValues += value;
-            if (value > scoreTile) {
-                biggerValues += value;
+            Integer tileValue = entry.getKey();
+            Integer tileCount = entry.getValue();
+            totalValues += tileCount;
+            if (tileValue > scoreTile) {
+                biggerValues += tileCount;
             }
         }
 
         double risk = (scoreTile * biggerValues) / (emptyNeighbors * totalValues);
-        if (tile.getPlayerId() == opponentId)
+        if (tile.getPlayerId() == opponentId) {
             return -risk;
-        else
+        } else {
             return risk;
+        }
 
     }
 
@@ -157,7 +159,7 @@ public class HeuristicPlayer implements AbstractPlayer {
         }
         // printHashMap(opponentsPool);
         Tile lastMoveTile = board.getTile(lastMove[0], lastMove[1]);
-        assert(opponentId == lastMoveTile.getPlayerId());
+        assert (opponentId == lastMoveTile.getPlayerId());
         Integer key = lastMoveTile.getScore();
         // decrease by 1.
         Integer value = opponentsPool.get(key) - 1;
