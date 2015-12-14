48b437b44dad48a48a332d346737477890124522
orestisf <orestisf1993@gmail.com>
2015-12-15 00:57:31 +0200

    Make auto-play.py os-agnostic

--- Proximity Part B/auto-play.py
+++ Proximity Part B/auto-play.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import os
 from subprocess import call
+from glob import glob
 import re
-RESULTS_FILE = r'Heuristic-old.txt'
-CMD = r"java -cp './runnable_lib/*:./bin' gr.auth.ee.dsproject.proximity.board.MainPlatform"
+RESULTS_FILE = r'Heuristic-Heuristic.txt'
+CMD_FORMAT = r'java -cp "{java_paths_str}" {main_path}'
+MAIN_PATH = r"gr.auth.ee.dsproject.proximity.board.MainPlatform"
 NUMBER_OF_ITERATIONS = 50
 
 def check_output():
@@ -11,13 +13,20 @@ def check_output():
         s = f.read()
     return [int(x) for x in re.search(r'(\d{1,4})-(\d{1,4})', s).groups()]
 
-if __name__ == '__main__':
+def main():
     scoreA = 0
     scoreB = 0
     winsA = 0
     winsB = 0
+    cur_dir = os.getcwd()
+    runnable_lib = os.path.join(cur_dir, 'runnable_lib', '*')
+    java_paths = glob(runnable_lib) + [os.path.join(cur_dir, 'bin')]
+    java_paths_str = os.path.pathsep.join(java_paths)
+    cmd = CMD_FORMAT.format(java_paths_str=java_paths_str, main_path=MAIN_PATH)
+    print(cmd)
+
     for iter_id in range(NUMBER_OF_ITERATIONS):
-        call(CMD, shell=True)
+        call(cmd, shell=True)
         result = check_output()
         if result[0] > result[1]:
             winsA += 1
@@ -27,3 +36,6 @@ if __name__ == '__main__':
         scoreB += result[1]
     print(scoreA, scoreB)
     print(winsA, winsB)
+
+if __name__ == '__main__':
+    main()
