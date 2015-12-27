38fcad82466637c7f996acf4a0e96176f3c4c689
orestisf <orestisf1993@gmail.com>
2015-12-27 06:40:24 +0200

    Write calculateRisk() doc

---
+++ Proximity Part B/doc/img/portrait_print.m
@@ -0,0 +1,7 @@
+function [] = portrait_print(h, filename)
+set(h, 'PaperType', 'a4','PaperOrientation', 'portrait')
+h.PaperUnits = 'centimeters';
+h.PaperPosition = [0 0 21 29.7/2];
+h.PaperPositionMode = 'manual';
+print(filename,'-dpdf','-r0')
+end
