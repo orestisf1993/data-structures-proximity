4021c665bbf6d30dad6a1bbc5f9b4b0d9d0910d9
orestisf <orestisf1993@gmail.com>
2016-01-09 15:02:57 +0200

    Add Node's to-be-implemented members and methods

--- Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/Node.java
+++ Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/Node.java
@@ -1,5 +1,112 @@
 package gr.auth.ee.dsproject.proximity.defplayers;
 
-public class Node {
+import java.util.ArrayList;
 
+import gr.auth.ee.dsproject.proximity.board.Board;
+
+public class Node {
+    Node parent;
+    ArrayList<Node> children;
+    int nodeDepth;
+    int[] nodeMove;
+    Board nodeBoard;
+    double nodeEvaluation;
+
+    Node() {
+    }
+
+    double evaluate() {
+        // TODO: call evaluator here
+        return 0.0;
+    }
+
+    /**
+     * @return the children
+     */
+    public ArrayList<Node> getChildren() {
+        return children;
+    }
+
+    /**
+     * @return the nodeBoard
+     */
+    public Board getNodeBoard() {
+        return nodeBoard;
+    }
+
+    /**
+     * @return the nodeDepth
+     */
+    public int getNodeDepth() {
+        return nodeDepth;
+    }
+
+    /**
+     * @return the nodeEvaluation
+     */
+    public double getNodeEvaluation() {
+        return nodeEvaluation;
+    }
+
+    /**
+     * @return the nodeMove
+     */
+    public int[] getNodeMove() {
+        return nodeMove;
+    }
+
+    /**
+     * @return the parent
+     */
+    public Node getParent() {
+        return parent;
+    }
+
+    /**
+     * @param children
+     *            the children to set
+     */
+    public void setChildren(ArrayList<Node> children) {
+        this.children = children;
+    }
+
+    /**
+     * @param nodeBoard
+     *            the nodeBoard to set
+     */
+    public void setNodeBoard(Board nodeBoard) {
+        this.nodeBoard = nodeBoard;
+    }
+
+    /**
+     * @param nodeDepth
+     *            the nodeDepth to set
+     */
+    public void setNodeDepth(int nodeDepth) {
+        this.nodeDepth = nodeDepth;
+    }
+
+    /**
+     * @param nodeEvaluation
+     *            the nodeEvaluation to set
+     */
+    public void setNodeEvaluation(double nodeEvaluation) {
+        this.nodeEvaluation = nodeEvaluation;
+    }
+
+    /**
+     * @param nodeMove
+     *            the nodeMove to set
+     */
+    public void setNodeMove(int[] nodeMove) {
+        this.nodeMove = nodeMove;
+    }
+
+    /**
+     * @param parent
+     *            the parent to set
+     */
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
 }
