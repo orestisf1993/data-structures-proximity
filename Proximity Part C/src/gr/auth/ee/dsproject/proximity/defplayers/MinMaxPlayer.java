f461129f376dfab7466234d8cb705b6c55f92bda
gkartzoni <gkartzoni@gmail.com>
2016-01-10 17:48:19 +0200

    Add chooseMinMaxMove()

--- Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/MinMaxPlayer.java
+++ Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/MinMaxPlayer.java
@@ -1,5 +1,7 @@
 package gr.auth.ee.dsproject.proximity.defplayers;
 
+import java.util.ArrayList;
+
 import gr.auth.ee.dsproject.proximity.board.Board;
 
 public class MinMaxPlayer implements AbstractPlayer {
@@ -12,6 +14,42 @@ public class MinMaxPlayer implements AbstractPlayer {
     public MinMaxPlayer(final Integer pid) {
         id = pid;
         name = "MinMaxPlayerTeam2";
+    }
+
+    private Node chooseMinMaxMove(Node node) {
+        ArrayList<Node> children = node.getChildren();
+        if (children == null) {
+            node.evaluate();
+            return node;
+        }
+        int depth = node.getNodeDepth();
+        double bestValue = 0;
+        // min
+        if (depth % 2 == 1) {
+            bestValue = Double.POSITIVE_INFINITY;
+            for (Node child : children) {
+                Node result = chooseMinMaxMove(child);
+                double evaluation = result.getNodeEvaluation();
+                if (evaluation < bestValue) {
+                    bestValue = evaluation;
+                }
+            }
+        }
+        // max
+        else {
+            bestValue = Double.NEGATIVE_INFINITY;
+            for (Node child : children) {
+                Node result = chooseMinMaxMove(child);
+                double evaluation = result.getNodeEvaluation();
+                if (evaluation > bestValue) {
+                    bestValue = evaluation;
+                }
+            }
+
+        }
+
+        node.setNodeEvaluation(bestValue);
+        return node;
     }
 
     public void createMySubTree(final Node parent, final int depth) {
@@ -27,14 +65,11 @@ public class MinMaxPlayer implements AbstractPlayer {
     }
 
     public String getName() {
-
         return name;
-
     }
 
     public int[] getNextMove(final Board board, final int randomNumber) {
         return null;
-
     }
 
     public int getNumOfTiles() {
@@ -46,9 +81,7 @@ public class MinMaxPlayer implements AbstractPlayer {
     }
 
     public void setId(final int id) {
-
         this.id = id;
-
     }
 
     public void setName(final String name) {
@@ -63,5 +96,4 @@ public class MinMaxPlayer implements AbstractPlayer {
     public void setScore(final int score) {
         this.score = score;
     }
-
 }
