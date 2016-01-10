70c79a5f895c9b310835e40ac716793615dc4b52
gkartzoni <gkartzoni@gmail.com>
2016-01-10 22:23:25 +0200

    Add chooseMinMaxMove()

--- Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/MinMaxPlayer.java
+++ Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/MinMaxPlayer.java
@@ -1,5 +1,7 @@
 package gr.auth.ee.dsproject.proximity.defplayers;
 
+import java.util.ArrayList;
+
 import gr.auth.ee.dsproject.proximity.board.Board;
 
 public class MinMaxPlayer implements AbstractPlayer {
@@ -12,6 +14,42 @@ public class MinMaxPlayer implements AbstractPlayer {
     public MinMaxPlayer(final Integer pid) {
         id = pid;
         name = "MinMaxPlayerTeam2";
+    }
+
+    private Node chooseMinMaxMove(Node node) {
+        ArrayList<Node> children = node.getChildren();
+        if (children == null) {
+            node.evaluate();
+            return node;
+        }
+        int depth = node.getNodeDepth();
+        double bestValue = 0;
+        // min
+        if (depth % 2 == 1) {
+            bestValue = Double.POSITIVE_INFINITY;
+            for (Node child : children) {
+                Node result = chooseMinMaxMove(child);
+                double evaluation = result.getNodeEvaluation();
+                if (evaluation < bestValue) {
+                    bestValue = evaluation;
+                }
+            }
+        }
+        // max
+        else {
+            bestValue = Double.NEGATIVE_INFINITY;
+            for (Node child : children) {
+                Node result = chooseMinMaxMove(child);
+                double evaluation = result.getNodeEvaluation();
+                if (evaluation > bestValue) {
+                    bestValue = evaluation;
+                }
+            }
+
+        }
+
+        node.setNodeEvaluation(bestValue);
+        return node;
     }
 
     public void createMySubTree(final Node parent, final int depth) {
@@ -58,9 +96,4 @@ public class MinMaxPlayer implements AbstractPlayer {
     public void setScore(final int score) {
         this.score = score;
     }
-
-    private int[] chooseMinMaxMove(Node root) {
-        return null;
-    }
-
 }
