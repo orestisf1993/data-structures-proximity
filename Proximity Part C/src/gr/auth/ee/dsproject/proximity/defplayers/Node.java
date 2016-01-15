7f8950b6310945b2bd63fbe506128bb6cfb4d659
orestisf <orestisf1993@gmail.com>
2016-01-15 17:56:13 +0200

    Remove logger from Node

--- Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/Node.java
+++ Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/Node.java
@@ -1,10 +1,6 @@
 package gr.auth.ee.dsproject.proximity.defplayers;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 import gr.auth.ee.dsproject.proximity.board.Board;
 import gr.auth.ee.dsproject.proximity.board.ProximityUtilities;
@@ -12,30 +8,6 @@ import gr.auth.ee.dsproject.proximity.board.Tile;
 
 // TODO: rename to correct class name for assignment.
 public class Node {
-    private static Logger logger;
-
-    // static initializer for logger
-    static {
-        if (logger == null) {
-            logger = Logger.getLogger("Node");
-            logger.setLevel(MinMaxPlayer.loggerLevel);
-            logger.setUseParentHandlers(false);
-
-            FileHandler fh;
-            try {
-                // This block configure the logger with handler and formatter
-                fh = new FileHandler("node.log");
-                logger.addHandler(fh);
-                SimpleFormatter formatter = new SimpleFormatter();
-                fh.setFormatter(formatter);
-            } catch (SecurityException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     /**
      *
      * @param tile
