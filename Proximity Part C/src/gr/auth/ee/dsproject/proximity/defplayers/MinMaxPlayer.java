f1bcad3a04978ae2b4a3aa03bd2b8bec3c00c442
orestisf <orestisf1993@gmail.com>
2016-01-18 00:52:10 +0200

    Add javadoc

--- Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/MinMaxPlayer.java
+++ Proximity Part C/src/gr/auth/ee/dsproject/proximity/defplayers/MinMaxPlayer.java
@@ -6,9 +6,27 @@ import gr.auth.ee.dsproject.proximity.board.Board;
 import gr.auth.ee.dsproject.proximity.board.ProximityUtilities;
 import gr.auth.ee.dsproject.proximity.board.Tile;
 
+/**
+ * The Class MinMaxPlayer implements a player that chooses the next move using
+ * the MinMax algorithm.
+ *
+ * @author Orestis Floros.
+ * @author Ioanna Gartzonika.
+ */
 public class MinMaxPlayer implements AbstractPlayer {
+
+    /** The max depth of our tree. */
     final private static int MAX_DEPTH = 2;
 
+    /**
+     * Find empty tiles.
+     *
+     * @param board
+     *            The current board.
+     * @param nextDepth
+     *            The depth for which we are searching the empty tiles.
+     * @return ArrayList with the empty tiles in the board.
+     */
     private static ArrayList<Tile> findEmptyTiles(final Board board, final int nextDepth) {
         // TODO: prefer corner tiles
         final ArrayList<Tile> emptyTiles = new ArrayList<Tile>();
@@ -29,15 +47,36 @@ public class MinMaxPlayer implements AbstractPlayer {
         return emptyTiles;
     }
 
+    /**
+     * Check if node level is ours.
+     *
+     * @param depth
+     *            the depth
+     * @return true if the depth is odd (ours), otherwise false.
+     */
     static boolean nodeLevelIsOurs(final int depth) {
         return depth % 2 == 1;
     }
 
+    /**
+     * Check if node level is ours.
+     *
+     * @param node
+     *            the node
+     * @return true if the depth of the node is odd (ours), otherwise false.
+     */
     static boolean nodeLevelIsOurs(final Node77968125 node) {
         final int depth = node.getNodeDepth();
         return nodeLevelIsOurs(depth);
     }
 
+    /**
+     * Reverse id.
+     *
+     * @param id
+     *            The id to reverse.
+     * @return The reversed id
+     */
     static int reverseId(final int id) {
         if (id != 1 && id != 2) {
             return id;
@@ -46,6 +85,16 @@ public class MinMaxPlayer implements AbstractPlayer {
         }
     }
 
+    /**
+     * check if tile is lone.
+     *
+     * @param tile
+     *            The tile to check.
+     * @param board
+     *            The current board.
+     * @return true when the tile doesn't have any non-empty neighbors,
+     *         otherwise false.
+     */
     static boolean tileIsLone(final Tile tile, final Board board) {
         final Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
         for (final Tile neighbor : neighbors) {
@@ -56,20 +105,43 @@ public class MinMaxPlayer implements AbstractPlayer {
         return true;
     }
 
+    /** The player's score. */
     private int score;
+
+    /** The player's id. */
     private int id;
+
+    /** The opponent's id. */
     private int opponentId;
+
+    /** The name of the player. */
     private String name;
+
+    /** The number of tiles. */
     private int numOfTiles;
 
+    /** The next numbers to be played. */
     private int[] nextNumbersToBePlayed;
 
+    /**
+     * Instantiates a new min max player.
+     *
+     * @param pid
+     *            the pid
+     */
     public MinMaxPlayer(final Integer pid) {
         id = pid;
         opponentId = reverseId(id);
         name = "MinMaxPlayerTeam2";
     }
 
+    /**
+     * Choose a move using MinMax.
+     *
+     * @param node
+     *            The parent node.
+     * @return a matrix with the coordinates of the best move.
+     */
     int[] chooseMinMaxMove(final Node77968125 node) {
         final ArrayList<Node77968125> children = node.getChildren();
         if (children.isEmpty()) {
@@ -95,6 +167,12 @@ public class MinMaxPlayer implements AbstractPlayer {
         return bestNode.getNodeMove();
     }
 
+    /**
+     * Creates the sub tree.
+     *
+     * @param parent
+     *            The parent node.
+     */
     void createSubTree(final Node77968125 parent) {
         // Find the empty tile spots of the board of the parent.
         final Board board = parent.getNodeBoard();
@@ -126,16 +204,32 @@ public class MinMaxPlayer implements AbstractPlayer {
     }
 
     /**
+     * Gets the id.
+     *
      * @return the id
      */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets the name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the next move.
+     *
+     * @param board
+     *            the board
+     * @param randomNumber
+     *            the random number
+     * @return int[] with our next move.
+     */
     public int[] getNextMove(final Board board, final int randomNumber) {
         nextNumbersToBePlayed = Board.getNextTenNumbersToBePlayed();
         assert (randomNumber == nextNumbersToBePlayed[0]);
@@ -146,15 +240,27 @@ public class MinMaxPlayer implements AbstractPlayer {
         return nextMove;
     }
 
+    /**
+     * Gets the num of tiles.
+     *
+     * @return numOfTiles
+     */
     public int getNumOfTiles() {
         return numOfTiles;
     }
 
+    /**
+     * Gets the score.
+     *
+     * @return score
+     */
     public int getScore() {
         return score;
     }
 
     /**
+     * Sets the id.
+     *
      * @param id
      *            the id to set
      */
@@ -163,14 +269,32 @@ public class MinMaxPlayer implements AbstractPlayer {
         this.opponentId = reverseId(id);
     }
 
+    /**
+     * Sets the name.
+     *
+     * @param name
+     *            the new name
+     */
     public void setName(final String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the num of tiles.
+     *
+     * @param tiles
+     *            the new num of tiles
+     */
     public void setNumOfTiles(final int tiles) {
         numOfTiles = tiles;
     }
 
+    /**
+     * Sets the score.
+     *
+     * @param score
+     *            the new score
+     */
     public void setScore(final int score) {
         this.score = score;
     }
